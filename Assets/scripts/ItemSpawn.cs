0286874299537a076fcea5d879f562928d9d6c83
ThereFox <sashaassasin2@gmail.com>
2022-09-01 22:59:59 +0400

    change spawner

--- Assets/scripts/ItemSpawn.cs
+++ Assets/scripts/ItemSpawn.cs
@@ -6,7 +6,7 @@ namespace ItemsSorting
 {
     public class ItemSpawn : MonoBehaviour
     {
-        [SerializeField] private GameObject _prefab;
+        [SerializeField] private List<GameObject> _prefabs;
         [SerializeField] private int _count = 5;
         [SerializeField] private Vector3 rotation;
         [SerializeField] private float spead;
@@ -16,13 +16,18 @@ namespace ItemsSorting
         public void Start()
         {
             transform = GetComponent<Transform>();
-            StartCoroutine(_spawnRandomElements().GetEnumerator());
+            StartCoroutine(_spawnRandomElements());
         }
-        private IEnumerable _spawnRandomElements()
+        private IEnumerator _spawnRandomElements()
         {
             for (int i = 0; i <= _count; i++)
             {
                 var randomSpeed = Random.Range(spead - (0.1f * spead), spead + (0.1f * spead));
+                
+                var prefabIndex = Random.Range(0, _prefabs.Count);
+                var _prefab = _prefabs[prefabIndex];
+
+
                 var rabdomPrefab = Instantiate(_prefab, transform.position, Quaternion.identity);
                 
                 rabdomPrefab.name = "draggableItem";
@@ -30,7 +35,7 @@ namespace ItemsSorting
                 rabdomPrefab.GetComponent<Rigidbody>().AddForce(rotation * spead, ForceMode.Force);
                 yield return new WaitForSeconds(0.75f);
             }
-            yield break;
+            //yield break;
         }
     }
 }
